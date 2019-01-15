56b6e69a708d74fb7c10fa623310d75bbffd6232
Patrick_Inshuti <pim219@nyu.edu>
2019-01-15 20:06:39 +0400

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express               = require("express"),
     LocalStrategy         = require("passport-local"),
     passportLocalMongoose = require("passport-local-mongoose")
 
+var Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost/sb8");
 
 app.set('view engine', 'ejs');
@@ -158,6 +159,53 @@ function isLoggedIn(req, res, next){
 //  content:"key to understanding sciences"
 //});
 
+
+
+const personSchema = mongoose.Schema({
+  _id: Schema.Types.ObjectId,
+  name: String,
+  age: Number,
+  stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]
+});
+
+const storySchema = mongoose.Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'Person' },
+  title: String,
+  fans: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
+});
+
+const Story = mongoose.model('Story', storySchema);
+const Person = mongoose.model('Person', personSchema);
+
+const author = new Person({
+  _id: new mongoose.Types.ObjectId(),
+  name: 'Ian Fleming',
+  age: 50
+});
+
+//console.log("here");
+author.save(function (err) {
+  if (err) return handleError(err);
+
+  const story1 = new Story({
+    title: 'Casino Royale',
+    author: author._id    // assign the _id from the person
+  });
+
+  story1.save(function (err) {
+    if (err) return handleError(err);
+    // thats it!
+  });
+});
+
+Story.
+  findOne({ title: 'Casino Royale' }).
+  populate('author').
+  exec(function (err, story) {
+    if (err) return handleError(err);
+    console.log('The author is %s', story.author.name);
+    // prints "The author is Ian Fleming"
+  });
 
 var port=4000;
 app.listen(port, function(req, res){
